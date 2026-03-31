73f1f1db8a6416531a152777b42e0d42a1bbbbd6
Aaron <a.zumstrull@gmx.de>
2026-03-31 16:26:01 +0200

    test

---
+++ script.js
@@ -0,0 +1,207 @@
+const PokemonNames = [];
+
+let searchInputValue = 0;
+
+function init() {
+    fetchPokemon();
+    fetchPokeNames();
+}
+
+async function fetchPokeNames() {
+    const NAMES_URL = 'https://pokeapi.co/api/v2/pokemon?limit=1025&offset=0';
+
+    let response = await fetch(NAMES_URL);
+    let responseToJson = await response.json();
+
+    for (let index = 0; index < responseToJson.results.length; index++) {
+        PokemonNames.push(responseToJson.results[index].name);
+    }
+}
+
+async function showSearchedPoke() {
+
+    let input = document.getElementById('input_field').value.toLowerCase();
+    let content = document.getElementById('content');
+
+
+    let results = PokemonNames.filter(name =>
+        name.toLowerCase().startsWith(input)
+    );
+
+    if (input.length >= 3) {
+
+        let selectedPokemon = document.getElementById('content');
+        selectedPokemon.innerHTML = '';
+
+        for (let index = 0; index < results.length; index++) {
+            const SOME_URL = `https://pokeapi.co/api/v2/pokemon/${results[index]}`;
+
+            let response = await fetch(SOME_URL);
+            let responseToJson = await response.json();
+            selectedPokemon.innerHTML += pokemonMainpageTemplate(responseToJson, index);
+
+            backgroundPokemonCard(responseToJson, index);
+
+        }
+        document.getElementById('alert_searchbar').style = '';
+    } else if (input.length < 3 && content.childElementCount < 20) {
+        if (input.length < searchInputValue) return
+        searchInputValue = input.length;
+        fetchPokemon();
+        currentIndex = 21;
+    } else if (input.length > 0 && input.length < 3) {
+        document.getElementById('alert_searchbar').style = 'display: flex';
+    } else if (input.length < 1) {
+        document.getElementById('alert_searchbar').style = '';
+    }
+
+}
+
+async function fetchPokemon() {
+    let allPokemon = document.getElementById('content');
+    allPokemon.innerHTML = '';
+
+
+    for (let index = 1; index < 21; index++) {
+        const SOME_URL = `https://pokeapi.co/api/v2/pokemon/${index}`;
+
+        let response = await fetch(SOME_URL);
+        let responseToJson = await response.json();
+        allPokemon.innerHTML += pokemonMainpageTemplate(responseToJson, index);
+
+        backgroundPokemonCard(responseToJson, index);
+    }
+}
+
+let currentIndex = 21;
+
+async function fetchMorePokemon() {
+    let allPokemon = document.getElementById('content');
+    let start = currentIndex;
+    let end = currentIndex + 19;
+
+    if (allPokemon.childElementCount < 20) {
+
+    } else {
+
+        for (let index = start; index <= end; index++) {
+            const SOME_URL = `https://pokeapi.co/api/v2/pokemon/${index}`;
+
+            let response = await fetch(SOME_URL);
+            let responseToJson = await response.json();
+            allPokemon.innerHTML += pokemonMainpageTemplate(responseToJson, index);
+            backgroundPokemonCard(responseToJson, index);
+        }
+        currentIndex += 20;
+    }
+}
+
+
+async function openOverlay(index) {
+    let pokemonCard = document.getElementById('overlay_content');
+    pokemonCard.innerHTML = "";
+
+    let results = PokemonNames;
+    let currentIndex = index - 1;
+
+    const SOME_URL = `https://pokeapi.co/api/v2/pokemon/${results[currentIndex]}`;
+    let response = await fetch(SOME_URL);
+    let responseToJson = await response.json();
+
+    document.getElementById('overlay').style = 'display: flex';
+    pokemonCard.innerHTML += pokemonCloseLook(responseToJson, currentIndex);
+    document.getElementById('stats_stats').style = 'display: none';
+
+    backgroundPokemonCardCloselook(responseToJson, currentIndex);
+    showAbilities(responseToJson);
+    let body = document.getElementById('body');
+    body.classList.add("overflow");
+}
+
+function showAbilities(responseToJson) {
+
+    for (let i = 0; i < responseToJson.abilities.length; i++) {
+
+        if (i === responseToJson.abilities.length - 1) {
+            document.getElementById('abilities_Id').innerHTML += responseToJson.abilities[i].ability.name;
+        } else {
+            document.getElementById('abilities_Id').innerHTML += responseToJson.abilities[i].ability.name + ", ";
+        }
+    }
+}
+
+async function openStats(index) {
+    document.getElementById('stats_inner').innerHTML = "";
+    document.getElementById('stats_main').style = 'display: none';
+    document.getElementById('stats_stats').style = '';
+
+    let currentIndex = index + 1;
+
+    const SOME_URL = `https://pokeapi.co/api/v2/pokemon/${currentIndex}`;
+    let response = await fetch(SOME_URL);
+    let responseToJson = await response.json();
+
+    for (let ix = 0; ix < responseToJson.stats.length; ix++) {
+
+        document.getElementById('stats_inner').innerHTML += templateInnerStats(responseToJson, ix);
+
+    }
+
+}
+
+function nextCard(index) {
+    let currentIndex = index + 2;
+    if (currentIndex <= 10500) {
+        closeOverlay();
+        openOverlay(currentIndex);
+    }
+    closeOverlay();
+}
+
+function previousCard(index) {
+    if (index >= 1) {
+        closeOverlay();
+        openOverlay(index);
+    }
+    closeOverlay();
+}
+
+function openMain() {
+    document.getElementById('stats_main').style = '';
+    document.getElementById('stats_stats').style = 'display: none';
+}
+
+function closeOverlay() {
+    document.getElementById('overlay').style = '';
+    let body = document.getElementById('body');
+    body.classList.remove("overflow");
+}
+
+function backgroundPokemonCard(responseToJson, index) {
+    let pokemonImgUrl = responseToJson.sprites.other["official-artwork"].front_default;
+    let pokemonImg = document.createElement('img');
+    pokemonImg.src = pokemonImgUrl;
+    document.getElementById(`pokemonImg_${index}`).appendChild(pokemonImg);
+
+    const className = responseToJson.types[0].type.name;
+
+    pokemonImg.classList.add("pokemon_img");
+    pokemonImg.classList.add(className);
+}
+
+function backgroundPokemonCardCloselook(responseToJson, index) {
+    let pokemonImgUrl = responseToJson.sprites.other["official-artwork"].front_default;
+    let pokemonImg = document.createElement('img');
+    pokemonImg.src = pokemonImgUrl;
+    document.getElementById(`pokemonImage_${index}`).appendChild(pokemonImg);
+
+    const className = responseToJson.types[0].type.name;
+
+    pokemonImg.classList.add("pokemon_img");
+    pokemonImg.classList.add(className);
+}
+
+function disableCloseoverlay(event) {
+    event.stopPropagation("onclick");
+}
+
---
+++ templates.js
@@ -0,0 +1,69 @@
+function pokemonMainpageTemplate(pokemon, index) {
+    return `
+        <div onclick=openOverlay(${pokemon.id}) class="pokemon_card">
+            <div>
+                <span>${pokemon.id}</span>
+                <span>${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</span>
+            </div>
+            <div id="pokemonImg_${index}"></div>
+            <div>
+                <img>
+                <img>
+            </div>
+        </div>
+    `;
+}
+
+function pokemonCloseLook(pokemon, index) {
+    return `
+        <div onclick="disableCloseoverlay(event)" class="pokemon_card_close pokemon_card">
+            <div>
+                <span>${pokemon.id}</span>
+                <span>${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</span>
+            </div>
+            <div class="style_pokemon_closelook" id="pokemonImage_${index}"></div>
+            <div class="space_around">
+                <div class="style_onclick_stats" onclick="openMain()">main</div>
+                <div class="style_border_stats"></div>
+                <div class="style_onclick_stats" onclick="openStats(${index})">stats</div>
+            </div>
+            <div class="style_div_underline">
+                <div class="style_underline"></div>
+                <div class="style_underline"></div>
+            </div>
+            <div id="stats_main" class="backgroundcolor_stats">
+                <div>
+                    <span>height</span>
+                    <span>: ${pokemon.height / 10 + ' m'}</span>
+                </div>
+                <div>
+                    <span>weight</span>
+                    <span>: ${pokemon.weight / 10 + ' kg'}</span>
+                </div>
+                <div>
+                    <span>base experience</span>
+                    <span>: ${pokemon.base_experience}</span>
+                </div>
+                <div>
+                    <span>abilities</span>
+                    <span id="abilities_Id">: </span>
+                </div>
+            </div>
+            <div id="stats_stats" class="backgroundcolor_stats">
+                <div id="stats_inner">
+                </div>
+            </div>
+            <div class="button-previous" onclick="previousCard(${index})"><</div>
+            <div class="button-next" onclick="nextCard(${index})">></div>
+        </div>
+    `;
+}
+
+function templateInnerStats(pokemon, ix) {
+    return `
+        <div>
+            <span>${pokemon.stats[ix].stat.name}</span>
+            <span id="stats_${ix}">: ${pokemon.stats[ix].base_stat}</span>
+        </div>
+    `;
+}
