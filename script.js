d9de7152ccecf92eacb965242f091b2f89cde427
Aaron <a.zumstrull@gmx.de>
2026-04-13 15:36:43 +0200

    code shortened 14 lines max/function

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ async function showSpinnerAndLoadPokemon() {
 
 async function showSpinnerAndLoadMorePokemon() {
     showLoadingSpinner();
-    await fetchMorePokemon();
+    await countAndFetchMorePokemon();
     disableLoadingSpinner();
 }
 
@@ -52,30 +52,10 @@ async function fetchPokeNames() {
 }
 
 async function showSearchedPoke() {
-
     let input = document.getElementById('input_field').value.toLowerCase();
     let content = document.getElementById('content');
-
-    let results = PokemonNames.filter(name =>
-        name.toLowerCase().startsWith(input)
-    );
-
     if (input.length >= 3) {
-
-        let selectedPokemon = document.getElementById('content');
-        selectedPokemon.innerHTML = '';
-
-        for (let index = 0; index < results.length; index++) {
-            const SOME_URL = `https://pokeapi.co/api/v2/pokemon/${results[index]}`;
-
-            let response = await fetch(SOME_URL);
-            let responseToJson = await response.json();
-            selectedPokemon.innerHTML += pokemonMainpageTemplate(responseToJson, index);
-
-            backgroundPokemonCard(responseToJson, index);
-
-        }
-        document.getElementById('alert_searchbar').style = '';
+        await searchPoke();
     } else if (input.length < 1 && content.childElementCount < 20) {
         fetchPokemon();
         currentIndex = 21;
@@ -85,23 +65,42 @@ async function showSearchedPoke() {
     } else if (input.length < 1) {
         document.getElementById('alert_searchbar').style = 'display: flex';
     }
+}
 
+async function searchPoke() {
+    let input = document.getElementById('input_field').value.toLowerCase();
+    let results = PokemonNames.filter(name =>
+        name.toLowerCase().startsWith(input));
+    let selectedPokemon = document.getElementById('content');
+    selectedPokemon.innerHTML = '';
+    for (let index = 0; index < results.length; index++) {
+        const SOME_URL = `https://pokeapi.co/api/v2/pokemon/${results[index]}`;
+        let response = await fetch(SOME_URL);
+        let responseToJson = await response.json();
+        selectedPokemon.innerHTML += pokemonMainpageTemplate(responseToJson, index);
+        backgroundPokemonCard(responseToJson, index);
+    }
+    document.getElementById('alert_searchbar').style = '';
 }
 
 async function fetchPokemon() {
     let allPokemon = document.getElementById('content');
     allPokemon.innerHTML = '';
-
-
     for (let index = 1; index < 21; index++) {
         const SOME_URL = `https://pokeapi.co/api/v2/pokemon/${index}`;
-
         let response = await fetch(SOME_URL);
         let responseToJson = await response.json();
         allPokemon.innerHTML += pokemonMainpageTemplate(responseToJson, index);
-
         backgroundPokemonCard(responseToJson, index);
         currentIndexObject.value = 21;
+    }
+}
+
+async function countAndFetchMorePokemon() {
+    let allPokemon = document.getElementById('content');
+    if (allPokemon.childElementCount < 20) {
+    } else {
+        await fetchMorePokemon();
     }
 }
 
@@ -109,40 +108,29 @@ async function fetchMorePokemon() {
     let allPokemon = document.getElementById('content');
     let start = currentIndex;
     let end = currentIndex + 19;
-
-    if (allPokemon.childElementCount < 20) {
-
-    } else {
-
-        for (let index = start; index <= end; index++) {
-            const SOME_URL = `https://pokeapi.co/api/v2/pokemon/${index}`;
-
-            let response = await fetch(SOME_URL);
-            let responseToJson = await response.json();
-            allPokemon.innerHTML += pokemonMainpageTemplate(responseToJson, index);
-            backgroundPokemonCard(responseToJson, index);
-        }
-        currentIndex += 20;
-        currentIndexObject.value = currentIndex;
+    for (let index = start; index <= end; index++) {
+        const SOME_URL = `https://pokeapi.co/api/v2/pokemon/${index}`;
+        let response = await fetch(SOME_URL);
+        let responseToJson = await response.json();
+        allPokemon.innerHTML += pokemonMainpageTemplate(responseToJson, index);
+        backgroundPokemonCard(responseToJson, index);
     }
+    currentIndex += 20;
+    currentIndexObject.value = currentIndex;
 }
 
 
 async function openOverlay(index) {
     let pokemonCard = document.getElementById('overlay_content');
     pokemonCard.innerHTML = "";
-
     let results = PokemonNames;
     let currentIndex = index - 1;
-
     const SOME_URL = `https://pokeapi.co/api/v2/pokemon/${results[currentIndex]}`;
     let response = await fetch(SOME_URL);
     let responseToJson = await response.json();
-
     document.getElementById('overlay').style = 'display: flex';
     pokemonCard.innerHTML += pokemonCloseLook(responseToJson, currentIndex);
     document.getElementById('stats_stats').style = 'display: none';
-
     backgroundPokemonCardCloselook(responseToJson, currentIndex);
     showAbilities(responseToJson);
     let body = document.getElementById('body');
@@ -150,9 +138,7 @@ async function openOverlay(index) {
 }
 
 function showAbilities(responseToJson) {
-
     for (let i = 0; i < responseToJson.abilities.length; i++) {
-
         if (i === responseToJson.abilities.length - 1) {
             document.getElementById('abilities_Id').innerHTML += responseToJson.abilities[i].ability.name;
         } else {
@@ -165,19 +151,13 @@ async function openStats(index) {
     document.getElementById('stats_inner').innerHTML = "";
     document.getElementById('stats_main').style = 'display: none';
     document.getElementById('stats_stats').style = '';
-
     let currentIndex = index + 1;
-
     const SOME_URL = `https://pokeapi.co/api/v2/pokemon/${currentIndex}`;
     let response = await fetch(SOME_URL);
     let responseToJson = await response.json();
-
     for (let ix = 0; ix < responseToJson.stats.length; ix++) {
-
         document.getElementById('stats_inner').innerHTML += templateInnerStats(responseToJson, ix);
-
     }
-
 }
 
 function nextCard(index) {
@@ -213,14 +193,11 @@ function backgroundPokemonCard(responseToJson, index) {
     let pokemonImg = document.createElement('img');
     pokemonImg.src = pokemonImgUrl;
     document.getElementById(`pokemonImg_${index}`).appendChild(pokemonImg);
-
     const className = responseToJson.types[0].type.name;
     let pokeTypes = responseToJson.types;
-
     for (let i = 0; i < pokeTypes.length; i++) {
         document.getElementById(`poke_types_${index}`).innerHTML += templatePokemonTypes(pokeTypes, i);
     }
-
     pokemonImg.classList.add("pokemon_img");
     pokemonImg.classList.add(className);
 }
@@ -230,9 +207,7 @@ function backgroundPokemonCardCloselook(responseToJson, index) {
     let pokemonImg = document.createElement('img');
     pokemonImg.src = pokemonImgUrl;
     document.getElementById(`pokemonImage_${index}`).appendChild(pokemonImg);
-
     const className = responseToJson.types[0].type.name;
-
     pokemonImg.classList.add("pokemon_img");
     pokemonImg.classList.add(className);
 }
@@ -241,6 +216,6 @@ function disableCloseoverlay(event) {
     event.stopPropagation("onclick");
 }
 
-function reloadPage(){
+function reloadPage() {
     location.reload();
 }
